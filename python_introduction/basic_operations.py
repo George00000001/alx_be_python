70b79ab40e3bcfdf8f1426acbac9667ee58848c8
George <george.milad0001@gmail.com>
2025-07-11 20:24:29 +0300

    calculateeeeeee

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,4 +4,3 @@ number2 = 5
 print("Addition of number1 and number2 is", (number1 + number2))
 print("Subtraction of number1 and number2 is", (number1 - number2))
 print("Multiplication of number1 and number2 is", (number1 * number2))
-print(number1 + number2)
