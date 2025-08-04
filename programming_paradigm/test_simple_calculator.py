98da157a473afac41a184fa44173a412dfdca07b
George <george.milad0001@gmail.com>
2025-08-04 20:13:08 +0300

    simple calc

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -22,8 +22,8 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-5, 5), -25)
 
-    def test_divide(self):
+    def test_division(self):
         #"""Test the divide method."""
         self.assertEqual(self.calc.divide(9, 3), 3)
-        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(10, 0), 5)
 
